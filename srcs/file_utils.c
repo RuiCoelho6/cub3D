646bbf42f629c60626ac889c620b0572eee08b9f
Pedro Andre Passos <ppassos@c2r4s4.42porto.com>
2025-06-11 12:23:05 +0100

    p2 parsing

---
+++ srcs/file_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppassos <ppassos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 12:00:09 by ppassos           #+#    #+#             */
+/*   Updated: 2025/06/11 12:01:08 by ppassos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+char	*ft_strjoin_free(char *str1, char *str2)
+{
+	char	*buffer;
+
+	buffer = ft_strjoin(str1, str2);
+	free(str1);
+	return (buffer);
+}
+
+char	**open_map(char *filename)
+{
+	int		fd;
+	char	*line;
+	char	*map;
+	char	**final_map;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (ft_printf("Error: Could not open file\n"), NULL);
+	map = get_next_line(fd);
+	if (!map)
+		return (ft_printf("Error: Empty file\n"), close(fd), NULL);
+	line = get_next_line(fd);
+	while (line)
+	{
+		map = ft_strjoin_free(map, line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	final_map = ft_split(map, '\n');
+	free(map);
+	if (final_map == NULL)
+		return (ft_printf("Error: Map empty\n"), NULL);
+	return (final_map);
+}
