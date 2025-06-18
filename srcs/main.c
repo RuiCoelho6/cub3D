29c919e242d4f22bdd206dbf7a163654bd00773b
Rui Pires <rpires-c@c1r13s5.42porto.com>
2025-06-18 12:31:37 +0100

    still has seg fault

--- srcs/main.c
+++ srcs/main.c
@@ -6,60 +6,57 @@
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 11:03:14 by rpires-c          #+#    #+#             */
-/*   Updated: 2025/06/18 11:22:22 by rpires-c         ###   ########.fr       */
+/*   Updated: 2025/06/18 12:26:01 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int     key_hook(int keycode, t_player *player, t_data *data)
+int	key_hook(int keycode, t_player *player, t_data *data)
 {
-
-    if (keycode == 65307) // ESC key
-        exit(0);
-    else if (keycode == 119) // W key - move forward
-    {
-        player->pos_x += cos(player->angle) * 5;
-        player->pos_y += sin(player->angle) * 5;
-    }
-    else if (keycode == 115) // S key - move backward
-    {
-        player->pos_x -= cos(player->angle) * 5;
-        player->pos_y -= sin(player->angle) * 5;
-    }
-    else if (keycode == 97) // A key - turn left
-        player->angle -= 0.1;
-    else if (keycode == 100) // D key - turn right
-        player->angle += 0.1;
-    
-    player->angle = normalize_angle(player->angle);
-    render_scene(player, data);
-    return (0);
+	if (keycode == 65307) // ESC key
+		exit(0);
+	else if (keycode == 119) // W key - move forward
+	{
+		player->pos_x += cos(player->angle) * 5;
+		player->pos_y += sin(player->angle) * 5;
+	}
+	else if (keycode == 115) // S key - move backward
+	{
+		player->pos_x -= cos(player->angle) * 5;
+		player->pos_y -= sin(player->angle) * 5;
+	}
+	else if (keycode == 97) // A key - turn left
+		player->angle -= 0.1;
+	else if (keycode == 100) // D key - turn right
+		player->angle += 0.1;
+	player->angle = normalize_angle(player->angle);
+	render_scene(player, data);
+	return (0);
 }
 
 // Initialize map
-void    init_map(t_data *data)
+void	init_map(t_data *data)
 {
-    int map_data[MAP_SIZE][MAP_SIZE] = {
-        {1,1,1,1,1,1,1,1},
-        {1,0,0,0,0,0,0,1},
-        {1,0,1,0,0,1,0,1},
-        {1,0,0,0,0,0,0,1},
-        {1,0,1,0,0,1,0,1},
-        {1,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1}
-    };
-    int i, j;
-    
-    for (i = 0; i < MAP_SIZE; i++)
-    {
-        for (j = 0; j < MAP_SIZE; j++)
-            data->map.map[i][j] = map_data[i][j];
-    }
-    data->map.size = 64;
-    data->map.width = MAP_SIZE;
-    data->map.height = MAP_SIZE;
+	int map_data[MAP_SIZE][MAP_SIZE] = {
+		{1,1,1,1,1,1,1,1},
+		{1,0,0,0,0,0,0,1},
+		{1,0,1,0,0,1,0,1},
+		{1,0,0,0,0,0,0,1},
+		{1,0,1,0,0,1,0,1},
+		{1,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1}
+	};
+	int i, j;
+	for (i = 0; i < MAP_SIZE; i++)
+	{
+		for (j = 0; j < MAP_SIZE; j++)
+			data->map.map[i][j] = map_data[i][j];
+	}
+	data->map.size = 64;
+	data->map.width = MAP_SIZE;
+	data->map.height = MAP_SIZE;
 }
 
 // Main function
@@ -67,12 +64,6 @@ int	main(void)
 {
 	t_data		data;
 	t_player	player;
-
-	data.mlx_ptr = mlx_init();
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Raycaster");
-	data.img->ptr = mlx_new_image(data.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
-	data.img->addr = mlx_get_data_addr(data.img, &data.img->bits_per_pixel, 
-									&data.img->line_length, &data.img->endian);
 
 	init_window(&data);
 	init_map(&data);
--- srcs/window_controls.c
+++ srcs/window_controls.c
@@ -6,7 +6,7 @@
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 10:59:14 by rpires-c          #+#    #+#             */
-/*   Updated: 2025/06/18 11:20:44 by rpires-c         ###   ########.fr       */
+/*   Updated: 2025/06/18 12:30:07 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 void	init_window(t_data *data)
 {
-	data->img = (t_img_data *)ft_calloc(1, sizeof(t_img_data));
-	data->img->bits_per_pixel = 0;
-	data->img->line_length = 0;
+	data->img = ft_calloc(1, sizeof(t_img_data));
+	if (!data->img)
+		return ;
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		return ;
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WIN_WIDTH, WIN_HEIGHT,
-			"cub3D");
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "cub3D");
 	if (!data->win_ptr)
 		return ;
 	data->img->ptr = mlx_new_image(data->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	if (!data->img->ptr)
 		return ;
 	data->img->addr = mlx_get_data_addr(data->img->ptr,
-			&data->img->bits_per_pixel,
-			&data->img->line_length, &data->img->endian);
+		&data->img->bits_per_pixel,
+		&data->img->line_length,
+		&data->img->endian);
+	if (!data->img->addr)
+	{
+		write(2, "Error: Failed to get image data address.\n", 41);
+		exit(EXIT_FAILURE);
+	}
 }
+
 
 void	my_mlx_pixel_put(t_img_data *img, int x, int y, int color)
 {
