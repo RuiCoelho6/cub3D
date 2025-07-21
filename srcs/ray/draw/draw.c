a1036999cb46dd6f1732470461b23e428fbb511a
Rui Pires <rpires-c@c2r4s2.42porto.com>
2025-07-21 11:55:20 +0100

    textures fixed

--- srcs/ray/draw/draw.c
+++ srcs/ray/draw/draw.c
@@ -6,21 +6,31 @@
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 10:02:48 by rpires-c          #+#    #+#             */
-/*   Updated: 2025/07/09 16:50:32 by rpires-c         ###   ########.fr       */
+/*   Updated: 2025/07/21 11:50:17 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../main.h"
 
 t_texture_params	calculate_texture_params(t_data *data,
-		t_wall wall, t_ray_result ray_result)
+		t_ray_result ray_result)
 {
 	t_texture_params	params;
+	float				full_wall_height;
+	float				theoretical_wall_start;
+	float				pixels_cut_off_top;
 
 	params.tex_x = calculate_texture_x(ray_result, data);
-	params.texture_step = (float)data->texture.height / wall.height;
-	params.texture_pos = (wall.start - (WIN_HEIGHT / 2)
-			+ wall.height / 2) * params.texture_step;
+	full_wall_height = (float)data->map.size * WIN_HEIGHT / ray_result.distance;
+	params.texture_step = (float)data->texture.height / full_wall_height;
+	theoretical_wall_start = (WIN_HEIGHT - full_wall_height) / 2.0f;
+	if (theoretical_wall_start < 0)
+	{
+		pixels_cut_off_top = -theoretical_wall_start;
+		params.texture_pos = pixels_cut_off_top * params.texture_step;
+	}
+	else
+		params.texture_pos = 0;
 	return (params);
 }
 
@@ -32,7 +42,7 @@ void	draw_textured_wall(t_data *data, int x,
 	int					y;
 	int					color;
 
-	params = calculate_texture_params(data, wall, ray_result);
+	params = calculate_texture_params(data, ray_result);
 	y = wall.start;
 	while (y < wall.end)
 	{
